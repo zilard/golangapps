384a7b9711fd764f4f80f2098009a9981ece451e
zilard <zilard.cherry@gmail.com>
2022-06-05 02:11:51 +0200

    collecting the levels of a binary tree in lists

--- levellinkedlist/main.go
+++ levellinkedlist/main.go
@@ -13,17 +13,36 @@ type BinaryTreeNode struct {
         Right *BinaryTreeNode
 }
 
+/*
+
+type Element struct {
+	Value any
+}
+
+
+               i2         3
+              / \        / \
+             i1  i4     2   5
+            /   /      /   /
+           i0  i3     1   4
+
+*/
+
+
 
 
 
 func CreateLevelLinkedList(root *BinaryTreeNode) []*list.List {
 	var result []*list.List
+	var resslice [][]int
 
 	current := list.New()
+	curslice := []int{}
 
 	// PushBack inserts a new element at the back of a list
 	if root != nil {
 		current.PushBack(root)
+		curslice = append(curslice, root.Value.(int))
 	}
 
 
@@ -31,26 +50,48 @@ func CreateLevelLinkedList(root *BinaryTreeNode) []*list.List {
 
 		// append -> appends new elements to a slice
 		result = append(result, current)
+		resslice = append(resslice, curslice)
 
 		parents := current
 		current = list.New()
+		curslice = []int{}
 
-		// Front returns the first element of list or nil if the list is empty.
+		// func (l *List) Front() *Element -  Front() returns the first element of list or nil if the list is empty.
+		// type List struct {..} - List represents a doubly linked list.
+		// type Element struct {..}  - Element is an element of a linked list.
+		// func (e *Element) Next() *Element - Next returns the next list element or nil.
+
 		for parent := parents.Front(); parent != nil; parent = parent.Next() {
 
 			node := parent.Value.(*BinaryTreeNode)  // explicit type casting
 
 			if node.Left != nil {
 				current.PushBack(node.Left)
+				curslice = append(curslice, node.Left.Value.(int))
 			}
 
 			if node.Right != nil {
 				current.PushBack(node.Right)
+                                curslice = append(curslice, node.Right.Value.(int))
 			}
 
 		}
 
+		// --------------
+		/*
+		curslice := []int{}
+		for c := current.Front(); c != nil; c = c.Next() {
+			//fmt.Printf("%T - %T\n", c, c.Value)
+                        v := c.Value.(*BinaryTreeNode).Value
+			curslice = append(curslice, v.(int))
+		}
+		fmt.Printf("curslice %v\n", curslice)
+		*/
+		// ---------------
+
 	}
+
+	fmt.Printf("resslice: %v\n", resslice)
 
 	return result
 
@@ -79,6 +120,8 @@ func IntToInterfaceSlice(array []int) []interface{} {
 	return result
 }
 
+
+
 /*
 
    [1,2,3,4,5]
@@ -100,6 +143,7 @@ func IntToInterfaceSlice(array []int) []interface{} {
            i0  i3     1   4
 
 */
+
 
 
 func createMinimalBinarySubTreeFromArray(array []interface{},
@@ -126,15 +170,33 @@ func main() {
 
 	tree := GetBinaryTreeFromSortedArray(expected)
 
-
-	//listsArray := CreateLevelLinkedList(tree)
-
-
-	fmt.Printf("tree: %v\n", tree)
+        fmt.Printf("tree: %v\n", tree)
 
 
+	listsArray := CreateLevelLinkedList(tree)
+
+	fmt.Printf("listsArray: %v\n", listsArray)
+
+
+	var list1Items []int
+	for item := listsArray[0].Front(); item != nil; item = item.Next() {
+		list1Items = append(list1Items, item.Value.(*BinaryTreeNode).Value.(int))
+	}
+	fmt.Printf("list1Items: %v\n", list1Items)
+
+
+        var list2Items []int
+        for item := listsArray[1].Front(); item != nil; item = item.Next() {
+                list2Items = append(list2Items, item.Value.(*BinaryTreeNode).Value.(int))
+        }
+        fmt.Printf("list2Items: %v\n", list2Items)
+
+
+        var list3Items []int
+        for item := listsArray[2].Front(); item != nil; item = item.Next() {
+                list3Items = append(list3Items, item.Value.(*BinaryTreeNode).Value.(int))
+        }
+        fmt.Printf("list3Items: %v\n", list3Items)
 
 }
-
-
 
