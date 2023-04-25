7ac1a7f97329e0cd5a40ac08d6e5781e1c3e177d
zilard <zilard.cherry@gmail.com>
2023-04-25 15:15:36 +0200

    blockchain app v1

---
+++ projectx/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	"github.com/zilard/golangapps/projectx/network"
+)
+
+func main() {
+
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		trRemote.SendMessage(trLocal.Addr(), []byte("Hello World"))
+		time.Sleep(1 * time.Second)
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
+
+}
