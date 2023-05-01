2d501fb7b988119cc53cba755888ea93e8ed695c
zilard <zilard.cherry@gmail.com>
2023-05-02 01:54:06 +0200

    first variant of game server

--- lowlatency-gameserver/game_server/main.go
+++ lowlatency-gameserver/game_server/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/anthdm/hollywood/actor"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/anthdm/hollywood/actor"
+	"github.com/gorilla/websocket"
+)
 
 type GameServer struct{}
 
@@ -8,8 +14,37 @@ func newGameServer() actor.Receiver {
 	return &GameServer{}
 }
 
-func (s *GameServer) Receive(c *actor.Context) {}
+func (s *GameServer) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Started:
+		s.startHTTP()
+		_ = msg
+	}
+}
+
+func (s *GameServer) startHTTP() {
+	fmt.Println("starting HTTP server on port 40000")
+	go func() {
+		http.HandleFunc("/ws", s.handleWS)
+		http.ListenAndServe(":40000", nil)
+
+	}()
+}
+
+// handles the upgrade of the websocket
+func (s *GameServer) handleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		fmt.Println("ws upgrade err:", err)
+		return
+	}
+	fmt.Print("new client trying to connect")
+	fmt.Print(conn)
+
+}
 
 func main() {
 	e := actor.NewEngine()
+	e.Spawn(newGameServer, "server")
+	select {}
 }
