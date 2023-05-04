3fc7db70c9e480325be80afc1eea6414323f3c93
zilard <zilard.cherry@gmail.com>
2023-05-04 19:00:27 +0200

    first composition

--- inheritance-struct-embedding/main.go
+++ inheritance-struct-embedding/main.go
@@ -1,18 +1,30 @@
 package main
 
+import "fmt"
+
+type Position struct {
+	x float64
+	y float64
+}
+
+func (p *Position) Move(x, y float64) {
+	p.x += x
+	p.y += y
+}
+
+func (p *Position) Teleport(x, y float64) {
+	p.x = x
+	p.y = y
+}
+
 type Player struct {
-	posX float64
-	posY float64
+	*Position
 }
 
-func (p *Player) Move(x, y float64) {
-	p.posX += x
-	p.posY += y
-}
-
-func (p *Player) Teleport(x, y float64) {
-	p.posX = x
-	p.posY = y
+func NewPlayer() *Player {
+	return &Player{
+		Position: &Position{},
+	}
 }
 
 type Enemy struct {
@@ -26,5 +38,7 @@ func (e *Enemy) Move(x, y float64) {
 }
 
 func main() {
+	player := NewPlayer()
 
+	fmt.Println(player.Position)
 }
