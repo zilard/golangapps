e514aab11bbb19c0dbc7726b802fd8ba79471120
zilard <zilard.cherry@gmail.com>
2023-05-13 01:17:13 +0200

    final adjustment done to unit test

--- ethquery/main_test.go
+++ ethquery/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -27,25 +26,24 @@ func TestGetEthBlockNumber(t *testing.T) {
 	// Define Fiber app.
 	app := fiber.New()
 
-	// Create route with GET method for test
-	app.Get("/api/getethblocknumber", func(c *fiber.Ctx) error {
-		// Return simple string as response
-		return c.SendString("get latest Eth block number")
-	})
+	s := &EthSession{
+		apiToken: "2cecb47e279c45ccb3bec53828c56b17",
+	}
+
+	EthRoutes(app, s)
 
 	// Iterate through the test cases
 	for _, test := range tests {
 		// Create a new http request with the route from the test case
-		req := httptest.NewRequest(http.MethodGet, test.route, nil)
+		req := httptest.NewRequest("GET", test.route, nil)
 
 		// Perform the request plain with the app,
 		// the second argument is a request latency
 		// (set to -1 for no latency)
-		resp, _ := app.Test(req, 1)
+		resp, _ := app.Test(req, -1)
 
 		// Verify, if the status code is as expected
 		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
-
 	}
 
 }
