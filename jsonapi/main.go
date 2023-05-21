23fc9b743696b048fb03f8ebd086aa2ca05253e5
zilard <zilard.cherry@gmail.com>
2023-05-22 00:17:13 +0200

    structure the API

--- jsonapi/main.go
+++ jsonapi/main.go
@@ -5,30 +5,59 @@ import (
 	"net/http"
 )
 
+var ErrUserInvalid = apiError{Err: "user not valid",
+	Status: http.StatusForbidden}
+
 type apiError struct {
-	err error
-	status int
+	Err    string
+	Status int
 }
 
 // implements the Error interface
 func (e apiError) Error() string {
-	return e.err
+	return e.Err
 }
 
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+// we have a centralized HTTP Handler
+func makeHTTPHandler(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			if e, ok := err.(apiError); ok {
+				writeJSON(w, e.Status, e)
+				return
+			}
+			writeJSON(w, http.StatusInternalServerError,
+				apiError{Err: "internal server",
+					Status: http.StatusInternalServerError})
+		}
+	}
+}
 
 func main() {
 
-	http.HandleFunc("/user", handleGetUserByID)
+	http.HandleFunc("/user", makeHTTPHandler(handleGetUserByID))
 
 	http.ListenAndServe(":3000", nil)
 }
 
-func handleGetUserByID(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		if err := writeJSON(w, http.StatusMethodNotAllowed, "any")
-		return
+type User struct {
+	ID    int
+	Valid bool
+}
 
+func handleGetUserByID(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != http.MethodGet {
+		return apiError{Err: "invalid method", Status: http.StatusMethodNotAllowed}
 	}
+
+	user := User{}
+	if !user.Valid {
+		return ErrUserInvalid
+	}
+
+	return writeJSON(w, http.StatusOK, user)
 }
 
 func writeJSON(w http.ResponseWriter, status int, v any) error {
