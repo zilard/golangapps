bcb96043b0ec397ef9c601105e8da24aad52ad93
zilard <zilard.cherry@gmail.com>
2023-05-23 11:48:00 +0200

    first version of CatFact REST API with MongoDB storage

--- catfacter/main.go
+++ catfacter/main.go
@@ -2,11 +2,52 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// BSON Binary serialization of JSON data, this how MongoDB stores data
+
+type CatFactWorker struct {
+	client *mongo.Client
+}
+
+func NewCatFactWorker(c *mongo.Client) *CatFactWorker {
+	return &CatFactWorker{
+		client: c,
+	}
+}
+
+func (cfw *CatFactWorker) start() error {
+	// coll := cfw.client.Database("catfact").Collection("facts")
+
+	// we dont want constantly polling that endpoint, hence need for ticker
+	ticker := time.NewTicker(2 * time.Second)
+
+	for {
+		// this link spits out random facts about Cats
+		resp, err := http.Get("https://catfact.ninja/fact")
+		if err != nil {
+			return err
+		}
+		var catFact bson.M // map[string]any // map[string]interface{}
+
+		if err := json.NewDecoder(resp.Body).Decode(&catFact); err != nil {
+			return err
+		}
+
+		fmt.Println(catFact)
+
+		<-ticker.C
+	}
+
+}
 
 func main() {
 	client, err := mongo.Connect(
@@ -16,5 +57,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(client)
+	worker := NewCatFactWorker(client)
+	worker.start()
+
 }
