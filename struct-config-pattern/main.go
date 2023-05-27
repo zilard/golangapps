a666781b028ad93f00c3d0e5502598ed36db771d
zilard <zilard.cherry@gmail.com>
2023-05-27 02:26:32 +0200

    implementing first draft of build config structs with functions

--- struct-config-pattern/main.go
+++ struct-config-pattern/main.go
@@ -2,23 +2,41 @@ package main
 
 import "fmt"
 
+type OptFunc func(*Opts)
+
 type Opts struct {
 	maxConn int
 	id      string
 	tls     bool
 }
 
+func defaultOpts() Opts {
+	return Opts{
+		maxConn: 10,
+		id:      "default",
+		tls:     false,
+	}
+}
+
+func withTLS(opts *Opts) {
+	opts.tls = true
+}
+
 type Server struct {
 	Opts
 }
 
-func newServer(opts Opts) *Server {
+func newServer(opts ...OptFunc) *Server {
+	o := defaultOpts()
+	for _, fn := range opts {
+		fn(&o)
+	}
 	return &Server{
-		Opts: opts,
+		Opts: o,
 	}
 }
 
 func main() {
-	s := newServer(Opts{})
+	s := newServer(withTLS)
 	fmt.Printf("%+v\n", s)
 }
