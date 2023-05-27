18f7951d4a5982e378c8c8c4747856c2ddb85b14
zilard <zilard.cherry@gmail.com>
2023-05-27 12:16:04 +0200

    start using channels and goroutines

--- data-aggregation-goroutines/main.go
+++ data-aggregation-goroutines/main.go
@@ -8,11 +8,20 @@ import (
 func main() {
 	start := time.Now()
 	userName := fetchUser()
-	likes := fetchUserLikes(userName)
-	match := fetchUserMatch(userName)
 
-	fmt.Println("likes: ", likes)
-	fmt.Println("match: ", match)
+	// will buffer my channel
+	// because unbuffered channel will always block
+	// until somebody is reading from it
+	respch := make(chan any, 2)
+
+	go fetchUserLikes(userName, respch)
+	go fetchUserMatch(userName, respch)
+
+	// in golang you can range over channels
+	for resp := range respch {
+		fmt.Println("resp: ", resp)
+	}
+
 	fmt.Println("took: ", time.Since(start))
 }
 
@@ -22,14 +31,14 @@ func fetchUser() string {
 	return "BOB"
 }
 
-func fetchUserLikes(userName string) int {
+func fetchUserLikes(userName string, respch chan any) {
 	time.Sleep(time.Millisecond * 150)
 
-	return 11
+	respch <- 11
 }
 
-func fetchUserMatch(userName string) string {
+func fetchUserMatch(userName string, respch chan any) {
 	time.Sleep(time.Millisecond * 100)
 
-	return "ANNA"
+	respch <- "ANNA"
 }
