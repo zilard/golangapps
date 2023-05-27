10b62020d5fe6854102dcd73bd8ec2df3af2c33c
zilard <zilard.cherry@gmail.com>
2023-05-27 12:32:09 +0200

    add waitgroup so we can wait until all async function transmit their response back to main

--- data-aggregation-goroutines/main.go
+++ data-aggregation-goroutines/main.go
@@ -2,24 +2,44 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	userName := fetchUser()
+	userName := fetchUser() // takes 100ms to execute, delay added
 
 	// will buffer my channel
 	// because unbuffered channel will always block
 	// until somebody is reading from it
 	respch := make(chan any, 2)
 
-	go fetchUserLikes(userName, respch)
-	go fetchUserMatch(userName, respch)
+	// use async wait group
+	wg := &sync.WaitGroup{}
+
+	// we have to workers that needs to communicate with the wait group
+	// telling it that they are done
+	wg.Add(2)
+	// each time we call wg.Done() it's going to substract from this number
+	// and when it goes to zero then wg.Wait() will unblock
+
+	// then we gonna do our work
+	// we do the wg.Done() in our asynchronous functions
+	// we need to provide the waitgroup as parameter to these functions
+	go fetchUserLikes(userName, respch, wg)
+	go fetchUserMatch(userName, respch, wg)
+
+	// right before we close the channel we call wg wait
+	// this is going to block until we have 2 wg.Done()
+	wg.Wait()
 
 	// we need to close the response channel, so that
 	// we dont have deadlock when we try to range over channel and wait for data
 	close(respch)
+
+	// we are closing the channel who guarantees that these guys
+	// fetchUserLikes and fetchUserMatch are already done
 
 	// in golang you can range over channels
 	// we gonna have a deadlock if we dont close the channel
@@ -46,14 +66,20 @@ func fetchUser() string {
 	return "BOB"
 }
 
-func fetchUserLikes(userName string, respch chan any) {
+func fetchUserLikes(userName string, respch chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 150)
 
 	respch <- 11
+
+	// we do the wg.Done() in our asynchronous functions
+	wg.Done()
 }
 
-func fetchUserMatch(userName string, respch chan any) {
+func fetchUserMatch(userName string, respch chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 100)
 
 	respch <- "ANNA"
+
+	// we do the wg.Done() in our asynchronous functions
+	wg.Done()
 }
