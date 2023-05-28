dc30d20ae4ac37157f32d9f30a8d9fa6df842c45
zilard <zilard.cherry@gmail.com>
2023-05-28 13:43:35 +0200

    grouping multiple constants on top of the file

--- idiomatic/main.go
+++ idiomatic/main.go
@@ -13,14 +13,18 @@ package main
 
 // in golang we treat constants just like other variables
 // no uppercase for constants
-
-const scalar = 0.1
+//const scalar = 0.1
 
 // if you want to export it then we can capitalize
 // the first letter
-const Scalar = 0.1
+// const Scalar = 0.1
 
 // if you wanna have multiple constants
+// then you need to group them, on top of the file
+const (
+	Scalar  = 0.1
+	Version = 0.1
+)
 
 func main() {
 
